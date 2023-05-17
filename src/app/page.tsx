ae04da39dd3994738b38cfd9dac72264ff5dc0b0
Nathan Florencio <nflorencio@showcasepro.com.br>
2023-05-17 16:52:36 -0300

    2:47:36.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,83 @@ import { Footer } from "@/components/Footer"
 import { Header } from "@/components/Header"
 import { Sidebar } from "@/components/Sidebar"
 import { Chat } from "@/types/Chat"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { v4 as uuidv4 } from 'uuid'
 
 const Page = () => {
   const [sidebarOpened, setSidebarOpened] = useState(false)
-  const [chatActive, setChatActive] = useState<Chat>({
-    id: '123',
-    title: 'Bla Blu',
-    messages: [
-      { id: '99', author: 'me', body: 'Opa, tudo bem? '},
-      { id: '100', author: 'ai', body: 'Tudo ótimo, em que posso te ajudar?' }
-    ]
-  })
+  const [chatList, setChatList] = useState<Chat[]>([])
+  const [chatActiveId, setChatActiveId] = useState<string>()
+  const [chatActive, setChatActive] = useState<Chat>()
   const [AILoading, setAILoading] = useState(false)
+
+  useEffect(() => {
+    setChatActive(chatList.find(item => item.id === chatActiveId))
+  }, [chatActiveId, chatList])
+
+  useEffect(() => {
+    if (AILoading) getAIResponse()
+  }, [AILoading])
 
   const openSidebar = () => setSidebarOpened(true)
   const closeSidebar = () => setSidebarOpened(false)
 
-  const handleClearConversations = () => {
+  const getAIResponse = () => {
+    setTimeout(() => {
+      let chatListClone = [...chatList]
+      let chatIndex = chatListClone.findIndex(item => item.id === chatActiveId)
+      if (chatIndex > -1) {
+        chatListClone[chatIndex].messages.push({
+          id: uuidv4(),
+          author: 'ai',
+          body: 'Aqui vai a resposta da AI :)'
+        })
+      }
+      setChatList(chatListClone)
+      setAILoading(false)
+    }, 2000)
+  }
 
+  const handleClearConversations = () => {
+    if (AILoading) return
+
+    setChatActiveId('')
+    setChatList([])
   }
 
   const handleNewChat = () => {
+    if (AILoading) return
 
+    setChatActiveId('')
+    closeSidebar()
   }
 
-  const handleSendMessage = () => {
+  const handleSendMessage = (message: string) => {
+    if (!chatActiveId) {
+      // Creating new chat
+      let newChatId = uuidv4()
+      setChatList([{ 
+        id: newChatId,
+        title: message,
+        messages: [
+          { id: uuidv4(), author: 'me', body: message }
+        ]
+      },...chatList])
 
+      setChatActiveId(newChatId)
+    } else {
+      // Updating existing chat
+      let chatListClone = [...chatList]
+      let chatIndex = chatListClone.findIndex(item => item.id === chatActiveId)
+      chatListClone[chatIndex].messages.push({
+        id: uuidv4(), 
+        author: 'me', 
+        body: message
+      })
+      setChatList(chatListClone)
+    }
+
+    setAILoading(true)
   }
 
   return (
@@ -52,7 +102,7 @@ const Page = () => {
           newChatClick={handleNewChat}
         />
 
-        <ChatArea chat={chatActive} />
+        <ChatArea chat={chatActive} loading={AILoading} />
 
         <Footer
           onSendMessage={handleSendMessage}
--- src/components/ChatMessageInput.tsx
+++ src/components/ChatMessageInput.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { KeyboardEvent, useEffect, useRef, useState } from "react"
 import IconSend from "./icons/IconSend"
 
 type Props = {
@@ -11,6 +11,22 @@ type Props = {
 export const ChatMessageInput = ({ disabled, onSend } : Props) => {
 
   const [text, setText] = useState('')
+  const textEl = useRef<HTMLTextAreaElement>(null)
+
+  useEffect(() => {
+    if (textEl.current) {
+      textEl.current.style.height = '0px'
+      let scrollHeight = textEl.current.scrollHeight
+      textEl.current.style.height = scrollHeight + 'px'
+    }
+  }, [text, textEl])
+
+  const handleTextKeyUp = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.code.toLowerCase() === 'enter' && !event.shiftKey) {
+      event.preventDefault()
+      handleSendMessage()
+    }
+  }
 
   const handleSendMessage = () => {
     if (!disabled && text.trim() !== '') {
@@ -24,13 +40,20 @@ export const ChatMessageInput = ({ disabled, onSend } : Props) => {
     ${disabled && 'opacity-50'}`}>
 
       <textarea
-        className="flex-1 border-0 bg-transparent resize-none outlined-none
+        ref={textEl}
+        className="flex-1 border-0 bg-transparent resize-none outline-none
         h-7 max-h-48 overflow-y-auto"
+        placeholder="Digite uma mensagem"
+        value={text}
+        onChange={e => setText(e.target.value)}
+        onKeyUp={handleTextKeyUp}
+        disabled={disabled}
       >
 
       </textarea>
 
-      <div onClick={handleSendMessage} className="">
+      <div onClick={handleSendMessage} className={`self-end p-1 cursor-pointer rounded
+      ${text.length ? 'opacity-100 hover:bg-black/20' : 'opacity-20'}`}>
         <IconSend width={14} height={14} />
       </div>
     </div>
